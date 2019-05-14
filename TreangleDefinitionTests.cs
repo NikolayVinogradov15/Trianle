d23dbc93d8b8ae555081b8669cf77c5810a5fb1c
NikolayVinogradov15 <50632801+NikolayVinogradov15@users.noreply.github.com>
2019-05-14 21:32:10 +0300

    Add files via upload
    TrianleDefinition необходимая библиотека для проверки типа треугольника по его сторонам. TriangleDefinitionTests - реализация теста для dll.

---
+++ TreangleDefinitionTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriangleDefinition.Tests
+{
+    [TestClass]
+    public class TreangleDefinitionTests
+    {
+        [TestMethod]
+        public void TriangleDefinition_acute()
+        {
+            // arrange
+
+            double a = 5;
+            double b = 5;
+            double c = 5;
+            string expected = "acute";
+
+            //act
+
+            string actual=Triangle.TriangleDefinition(a,b,c);
+
+            //assert
+
+            Assert.AreEqual(expected,actual);
+
+        }
+        [TestMethod]
+        public void TriangleDefinition_impossible()
+        {
+            // arrange
+
+            double a = 5;
+            double b = 5;
+            double c = 25;
+            string expected = "impossible";
+
+            //act
+
+            string actual = Triangle.TriangleDefinition(a, b, c);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TriangleDefinition_rectangular()
+        {
+            // arrange
+
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            string expected = "rectangular";
+
+            //act
+
+            string actual = Triangle.TriangleDefinition(a, b, c);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TriangleDefinition_obtuse()
+        {
+            // arrange
+
+            double a = 10;
+            double b = 10;
+            double c = 17.32;
+            string expected = "obtuse";
+
+            //act
+
+            string actual = Triangle.TriangleDefinition(a, b, c);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+public class Triangle
+    {
+        public static string TriangleDefinition(double a, double b, double c)
+        {
+            double[] max = { a,b,c};
+        for(int j=0; j<2;j++)
+            for (int i = 0; i < 2; i++)
+            if(max[i]<max[i+1])
+                Swap(ref max[i], ref max[i+1]);
+      
+            
+        Console.WriteLine(max[0] + "  " + max[1] + "  " + max[2]);
+            if (max[0] > max[1] + max[2]||a<=0||b<=0||c<=0)
+                return "impossible";
+            else
+            {
+                double temp = -max[0] * max[0] + max[1] * max[1] + max[2] * max[2];
+                if (temp > 0)
+                    return "acute";
+                else if (temp == 0)
+                    return "rectangular";
+                else
+                    return "obtuse";
+            }
+           
+        }
+        public static void Swap(ref double a,ref double b)
+        {
+            double c = a;
+            a = b;
+            b = c;
+        }
+    }
+
